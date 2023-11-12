8b2afa44d93b7d43cd2b6703e4a786a6c5c0897f
MrArtemSid <artem.sid.key@gmail.com>
2023-11-12 17:08:10 +0400

    GA-P61-S3-B3: Update SPD map

--- p61-s3-b3/early_init.c
+++ p61-s3-b3/early_init.c
@@ -39,11 +39,8 @@ void bootblock_mainboard_early_init(void)
 	ite_reg_write(SUPERIO_GPIO, 0xEF, 0x7E);
 }
 
-/* FIXME: Put proper SPD map here. */
 void mainboard_get_spd(spd_raw_data *spd, bool id_only)
 {
 	read_spd(&spd[0], 0x50, id_only);
-	read_spd(&spd[1], 0x51, id_only);
 	read_spd(&spd[2], 0x52, id_only);
-	read_spd(&spd[3], 0x53, id_only);
 }
